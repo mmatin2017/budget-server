a382e28a1a6c7204d625688729daabf005125620
Mohammed Matin <mmatin@uncc.edu>
2020-12-19 13:48:45 -0500

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require('mongodb').MongoClient;
 const bodyParser = require("body-parser");
 
 const app = express();
